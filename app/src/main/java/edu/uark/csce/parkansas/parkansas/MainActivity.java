1b526e2f062ae55f2084c2dffb51c944458d7f57
LOGIN <thang@email.uark.edu>
2015-02-12 16:00:01 -0600

    googlemap working

--- app/src/main/java/edu/uark/csce/parkansas/parkansas/MainActivity.java
+++ app/src/main/java/edu/uark/csce/parkansas/parkansas/MainActivity.java
@@ -9,6 +9,22 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.view.Menu;
+
 
 public class MainActivity extends Activity {
 
@@ -25,6 +41,8 @@ public class MainActivity extends Activity {
     // Map Variables
 //    MapKey mapKey;
 
+    private GoogleMap map;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,8 +54,29 @@ public class MainActivity extends Activity {
         loggedIn = false;
         skipLogin = sharedPreferences.getBoolean(ActivityUtils.SKIP_LOGIN, false);
 
-        if(!loggedIn && !skipLogin)
+        if (!loggedIn && !skipLogin)
             lauchLoginActivity();
+
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                .getMap();
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+        criteria.setCostAllowed(true);
+
+        // String provider = LocationManager.GPS_PROVIDER;
+        String provider = locationManager.getBestProvider(criteria, true);
+        Location loc = locationManager.getLastKnownLocation(provider);
+
+        updateWithNewLocation(loc);
+        //locationManager.requestLocationUpdates(provider, 2000, 10, locationListener);
+
     }
 
     @Override
@@ -54,9 +93,10 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_login: {
                 lauchLoginActivity();
                 return true;
@@ -77,9 +117,9 @@ public class MainActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        switch(requestCode){
+        switch (requestCode) {
             case ActivityUtils.LOGIN_RETURN: {
-                if(resultCode == Activity.RESULT_OK)
+                if (resultCode == Activity.RESULT_OK)
                     break;
                 else
                     break;
@@ -89,9 +129,50 @@ public class MainActivity extends Activity {
         }
     }
 
-    private void lauchLoginActivity()
-    {
+    private void lauchLoginActivity() {
         intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivityForResult(intent, ActivityUtils.LOGIN_RETURN);
+    }
+
+    private void updateWithNewLocation(Location loc) {
+        if (loc != null) {
+            double lat = loc.getLatitude();
+            double lng = loc.getLongitude();
+
+            LatLng location = new LatLng(lat, lng);
+            map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(location,
+                    16);
+            map.animateCamera(update);
+            map.addMarker(new MarkerOptions().position(location).title(
+                    "I am here!"));
+        }
+
+
+        LocationListener locationListener = new LocationListener() {
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onLocationChanged(Location location) {
+                updateWithNewLocation(location);
+            }
+        };
     }
 }
