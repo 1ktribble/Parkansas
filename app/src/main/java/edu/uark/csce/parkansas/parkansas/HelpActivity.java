0f2f6c86ca43f7a0ebd0c04a5d58d58f672aa708
LOGIN <thang@email.uark.edu>
2015-04-25 13:08:00 -0500

    Added Help
    -At the moment, there is contact message with an intent that launch GMAIL to send the developers emails.
    
    -On the next commit, planning on adding a navigation section which gives a descriptions of all the icons.

--- app/src/main/java/edu/uark/csce/parkansas/parkansas/HelpActivity.java
+++ app/src/main/java/edu/uark/csce/parkansas/parkansas/HelpActivity.java
@@ -1,37 +1,47 @@
 package edu.uark.csce.parkansas.parkansas;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class  HelpActivity extends Activity {
+//    ImageView alert;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        TextView contact = (TextView) findViewById(R.id.contactDescription);
+        contact.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent (Intent.ACTION_VIEW , Uri.parse("mailto:" + "team.m.parkansas@gmail.com"));
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Contact from App: ");
+                intent.putExtra(Intent.EXTRA_TEXT, "Greeting PARKansas Developers,\n\n");
+                startActivity(intent);
+            }
+        });
+
+//        alert = (ImageView) findViewById(R.id.alertIcon);
+//        tonyPic = (ImageView) findViewById(R.id.tonyPic);
+//        dakotaPic = (ImageView) findViewById(R.id.dakotaPic);
+//        kaiPic = (ImageView) findViewById(R.id.kaiPic);
+
+        setImages();
     }
 
+    private void setImages(){
+//        alert.setImageResource(R.drawable.ic_action_suspend_alert_no_trim_midpadding);
+//        tonyPic.setImageResource(R.drawable.ic_tony_picture);
+//        dakotaPic.setImageResource(R.drawable.ic_dakota_placeholder);
+//        kaiPic.setImageResource(R.drawable.ic_kai_picture);
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_help, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+
+
+    }
 }
