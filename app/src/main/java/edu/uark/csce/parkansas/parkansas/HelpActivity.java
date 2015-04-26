168ba7af537d5649dfeeffe244d7319ccdf63896
LOGIN <thang@email.uark.edu>
2015-04-26 01:32:46 -0500

    Added Navigation Guide to Help Activity

--- app/src/main/java/edu/uark/csce/parkansas/parkansas/HelpActivity.java
+++ app/src/main/java/edu/uark/csce/parkansas/parkansas/HelpActivity.java
@@ -5,43 +5,44 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridView;
 import android.widget.TextView;
 
 
 public class  HelpActivity extends Activity {
-//    ImageView alert;
-
+    GridView grid;
+    String[] description = {
+            "Alert Activity",
+            "Refresh Map",
+            "Filter Option",
+            "Parking Garages",
+            "Search",
+            "Center Map"};
+    int[] icon = {
+            R.drawable.ic_action_suspend_alert_no_trim_midpadding,
+            R.drawable.ic_refresh_map_no_trim_midpadding,
+            R.drawable.filters,
+            R.drawable.g,
+            R.drawable.ic_search,
+            R.drawable.center_map};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
+        Grid adapter = new Grid(HelpActivity.this, description, icon);
+        grid = (GridView) findViewById(R.id.grid);
+        grid.setAdapter(adapter);
+
         TextView contact = (TextView) findViewById(R.id.contactDescription);
         contact.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent intent = new Intent (Intent.ACTION_VIEW , Uri.parse("mailto:" + "team.m.parkansas@gmail.com"));
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("mailto:" + "team.m.parkansas@gmail.com"));
                 intent.putExtra(Intent.EXTRA_SUBJECT, "Contact from App: ");
                 intent.putExtra(Intent.EXTRA_TEXT, "Greeting PARKansas Developers,\n\n");
                 startActivity(intent);
             }
         });
-
-//        alert = (ImageView) findViewById(R.id.alertIcon);
-//        tonyPic = (ImageView) findViewById(R.id.tonyPic);
-//        dakotaPic = (ImageView) findViewById(R.id.dakotaPic);
-//        kaiPic = (ImageView) findViewById(R.id.kaiPic);
-
-        setImages();
-    }
-
-    private void setImages(){
-//        alert.setImageResource(R.drawable.ic_action_suspend_alert_no_trim_midpadding);
-//        tonyPic.setImageResource(R.drawable.ic_tony_picture);
-//        dakotaPic.setImageResource(R.drawable.ic_dakota_placeholder);
-//        kaiPic.setImageResource(R.drawable.ic_kai_picture);
-
-
-
     }
 }
